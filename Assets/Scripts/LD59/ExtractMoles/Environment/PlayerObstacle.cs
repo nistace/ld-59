4158df0a3e364a370263236382988acb26c28fc6
nathan.istace <nathan.istace@gmail.com>
2026-04-20 15:49:30 +0200

    Fixed some leaving the level because of missing colliders

--- Assets/Scripts/LD59/ExtractMoles/Environment/PlayerObstacle.cs
+++ Assets/Scripts/LD59/ExtractMoles/Environment/PlayerObstacle.cs
@@ -5,10 +5,11 @@ namespace LD59.ExtractMoles.Environment
    public class PlayerObstacle : MonoBehaviour
    {
       [SerializeField] private BoxCollider _boxCollider;
+      [SerializeField] private Color _gizmoColor = new(1, 0, 0, .3f);
 
       private void OnDrawGizmos()
       {
-         Gizmos.color = new Color( 1, 0, 0, .3f );
+         Gizmos.color = _gizmoColor;
          Gizmos.matrix = transform.localToWorldMatrix;
          Gizmos.DrawCube( _boxCollider.center, _boxCollider.size );
       }
